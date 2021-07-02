31ed6372fbde169a8fb0ca555a840ceaa0843930
James Goodman <43926484+SlothInABox@users.noreply.github.com>
2021-07-02 20:18:06 +0100

    Fixed challenge 2
    Balls now spawn from the top of the screen
    The player spawns dogs
    Balls are destroyed when coming into contact with dogs
    Objects are destroyed when leaving the bounds of the screen
    Ball prefabs are spawned randomly
    Time interval between spawns is randomised
    Player can no longer spam dogs

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float timeDelay = 1.0f;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spawn delay
+        timer += Time.deltaTime;
+        if (timer >= timeDelay)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 0.0f;
+            }
+        }
+    }
+}
