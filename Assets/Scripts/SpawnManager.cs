627d574078fa0351a3e0c3e99e9415c7ac3fc5dd
James Goodman <43926484+SlothInABox@users.noreply.github.com>
2021-07-02 19:44:57 +0100

    Added animal spawner
    Random animals now spawn at a random position when the "S" key is pressed
    The camera perspective has been changed to "Orthographic"

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Spawn random animal at random position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
